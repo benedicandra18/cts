b8681ed27bd736f0c9fb5545f91d23dbd5cd7ad5
benedicandra18 <74496610+benedicandra18@users.noreply.github.com>
2021-04-22 12:54:16 +0300

    State

---
+++ seminar9/src/ro/ase/cts/state/clase/Rezervata.java
@@ -0,0 +1,17 @@
+package ro.ase.cts.state.clase;
+
+public class Rezervata implements Stare{
+
+	@Override
+	public void schimbaStarea(Masa masa) {
+		if(masa.getStare() instanceof Libera) {
+			System.out.println("Ati rezervat masa cu codul "+masa.getNr());
+			masa.setStare(this);
+		}
+		else {
+			System.out.println("Masa cu codul "+masa.getNr()+" nu poate fi rezervata");
+		}
+		
+	}
+
+}
